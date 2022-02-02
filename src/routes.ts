c6ffccd03da6b67783ebf92dbaaf2b99d932b09f
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-01 21:58:11 -0300

    fix: added new repository and configuration routes

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "./repositories/SettingsRepository";
+
+
+const routes = Router();
+
+routes.post("/settings", async (request: Request, response: Response) => {
+
+    const { chat, username } = request.body;
+
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+        chat,
+        username
+    });
+
+    await settingsRepository.save(settings);
+
+    return response.json(settings);
+})
+
+export { routes }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { routes } from "./routes";
 import "./database";
+
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (request: Request, response: Response) => {
-    return response.json({ "message": "Hello world" })
-});
-
-app.post("/", (request: Request, response: Response) => {
-    return response.json({ "message": "Hello world" })
-});
+app.use(routes);
 
 app.listen(3000, () => console.log("Server running in port 3000"))
